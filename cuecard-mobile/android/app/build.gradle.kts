c7ec49d4f1565974057f70c5541d445d4f1cee88
thisisnsh <hadanis.singh@gmail.com>
2026-09-06 12:07:27 -0400

    Version 1.3.0

--- cuecard-mobile/android/app/build.gradle.kts
+++ cuecard-mobile/android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.thisisnsh.cuecard.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
