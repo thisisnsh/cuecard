ce6b9102b31a8e3cf872810c01d2530b67d7778b
thisisnsh <hadanis.singh@gmail.com>
2026-09-09 22:30:25 -0400

    Update versionCode to 6

--- cuecard-mobile/android/app/build.gradle.kts
+++ cuecard-mobile/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.thisisnsh.cuecard.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
