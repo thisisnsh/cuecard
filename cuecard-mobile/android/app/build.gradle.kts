cfda10ac124d44bc3f0cc3f428c6b88eaac03e59
thisisnsh <hadanis.singh@gmail.com>
2026-09-09 21:49:53 -0400

    Release 1.0.0 (5)

--- cuecard-mobile/android/app/build.gradle.kts
+++ cuecard-mobile/android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.thisisnsh.cuecard.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
