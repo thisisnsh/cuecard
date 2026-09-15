f5d1f0bf7d6395da725cd91ec068c63a7d831b41
thisisnsh <hadanis.singh@gmail.com>
2026-09-14 22:40:26 -0400

    Release 1.0.0 (9)

--- cuecard-mobile/android/app/build.gradle.kts
+++ cuecard-mobile/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.thisisnsh.cuecard.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
